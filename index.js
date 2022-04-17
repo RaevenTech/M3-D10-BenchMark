d364014bd641158e956400dd7b77f2706cd1db3c
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-04-17 20:46:08 +0100

    mainpage js

---
+++ index.js
@@ -0,0 +1,39 @@
+/*const nav = document.getElementById("nav");
+window.addEventListener("scroll", () => {
+    if (window.scrollY >= 100) {
+        nav.classList.add("nav-black");
+    } else {
+        nav.classList.remove("nav-black");
+    }
+});
+
+
+------------------------------------------------------------------------------
+
+
+
+    const options = {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization:
+                        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjU2YzZmYmE5MDIzOTAwMTVkOTY2MTAiLCJpYXQiOjE2NTAxOTQwMjQsImV4cCI6MTY1MTQwMzYyNH0.5ePw4ia-tdotlO6MuR6IOURL2rEGizqwsxDw6bdqGz",
+                },
+            };
+
+            window.onload = () => {
+                fetch(
+                    "https://striveschool-api.herokuapp.com/api/movies/",
+                    options
+                )
+                    .then((response) => response.json())
+                    .then((data) => {
+                        console.log(data);
+                    });
+            };
+
+
+
+
+
+*/
